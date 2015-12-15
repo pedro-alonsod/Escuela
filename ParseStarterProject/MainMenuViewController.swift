e6ef1958605434e7bc11b152a84ff88f1117ed7a
Pedro <pedro.alonsod@gmail.com>
2015-12-15 15:50:46 -0600

    test

--- ParseStarterProject/MainMenuViewController.swift
+++ ParseStarterProject/MainMenuViewController.swift
@@ -328,6 +328,8 @@ class MainMenuViewController: UIViewController {
                 tabBarVC.calificacionesAlumnos = []
                 
                 //changes here
+                //test
+                
                 
             }
             
